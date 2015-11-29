053833a63a476c5ce88eed203704d30bc3c1fca0
andrew <elitehunter92@gmail.com>
2015-11-29 12:22:08 -0500

    Added: - started password functionality here, moving to new branch

--- src/SignatureLauncher.java
+++ src/SignatureLauncher.java
@@ -1,10 +1,13 @@
 /**
  * Created by andrew on 11/5/15.
  */
+import java.util.Scanner;
+
 public class SignatureLauncher
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
         Signature sig = new Signature("./directory1", "./directory2");
         Validator val = new Validator("./directory1", "./directory2");
         sig.signatureAlgorithm();
